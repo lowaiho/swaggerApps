2ae29158262eaab6075f373c7797be9894c05ddb
LAPTOP-T4F8IVQ7\lowai <lowaiho@hotmail.com>
2022-01-17 15:59:21 +0800

    Add project files.

---
+++ swaggerApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace swaggerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// This is method summary I want displayed
+        /// </summary>
+        /// <param name="guid">guid as parameter</param>
+        /// <param name="page_number">Page number - defaults to 0</param>
+        /// <returns>List of objects returned</returns>
+        /// <response code="200">Pass 論文</response>
+        /// <response code="400">Fail 論文</response>
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
